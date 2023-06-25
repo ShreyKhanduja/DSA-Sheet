40f9c039d3e0a3b7cac192b3ddd0c9b403419d5d
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-06-25 21:05:45 +0530

    LinkedListset 2

---
+++ cycle_start.cpp
@@ -0,0 +1,22 @@
+
+ListNode *detectCycle(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while(fast && fast->next) {
+        slow = slow->next; // 1 step
+        fast = fast->next->next; // 2 steps
+
+        // cycle detected
+        if(slow == fast) {
+            // move fast to the head
+            fast = head;
+            while(slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
