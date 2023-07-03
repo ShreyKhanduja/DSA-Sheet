dcecb82087e5816d4e874d5ef0e53e3f49e1ee35
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-07-03 23:53:44 +0530

    Two Pointer

---
+++ max_consecutive_ones.cpp
@@ -0,0 +1,13 @@
+int findMaxConsecutiveOnes(vector<int> &nums) {
+    int max_count = 0, count = 0;
+
+    for(const int &x : nums) {
+        if(x == 1)
+            count++;
+        else    
+            count = 0;
+
+        max_count = max(max_count, count); 
+    }
+    return max_count;
+}
