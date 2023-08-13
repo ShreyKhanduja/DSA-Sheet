ffa1f01ce2477929eee5f668092aec9627403c1f
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-08-14 03:06:57 +0530

    Binary Search

---
+++ single_element.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+
+    int singleNonDuplicate(vector<int>& nums) {
+        // if the single element is at the last position
+        int start = 0, end = nums.size() - 2;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            // left part so move to the right
+            if(nums[mid] == nums[mid ^ 1])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return nums[start];
+    }
+};
