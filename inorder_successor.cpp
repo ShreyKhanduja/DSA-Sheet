048d4e731c380f270dfd6cc262f938add1818a91
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-09-26 03:55:22 +0530

    Binary Search Tree

---
+++ inorder_successor.cpp
@@ -0,0 +1,14 @@
+
+Node *inOrderSuccessor(Node *root, int x) {
+    Node *succ = NULL;
+    while(root) {
+        if(root->data > x->data) {
+            succ = root;
+            // x will lie in the left subtree
+            root = root->left;
+        }
+        else    
+            root = root->right;
+    }
+    return succ;
+}
