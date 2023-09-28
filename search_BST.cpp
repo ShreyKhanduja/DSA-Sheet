1131b86e8287440aea3efa76edbcaa32744ab454
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-09-29 03:56:24 +0530

    Binary Search Tree

---
+++ search_BST.cpp
@@ -0,0 +1,14 @@
+
+TreeNode *searchBST(TreeNode *root, int val) {
+    // base case
+    if(!root)
+        return NULL;
+    // condition check
+    if(root->val == val)
+        return root;
+    // recursive calls: narrow down the search space
+    if(val < root->val)
+        return searchBST(root->left, val);
+    else    
+        return searchBST(root->right, val);
+}
