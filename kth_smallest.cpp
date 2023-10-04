eaafae574c8b730856c4d193631a159b52ce359c
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-10-04 23:22:40 +0530

    kth smallest

---
+++ kth_smallest.cpp
@@ -0,0 +1,19 @@
+
+int kthSmallest(TreeNode *root, int k) {
+   stack<TreeNode*> s;
+   while(!s.empty() || root) {
+        // reach the left most node
+        while(root) {
+           s.push(root);
+           root = root->left;
+        }
+        // pop the left most node
+        root = s.top();
+        s.pop();
+        i(--k == 0)
+            return root->val;
+        // move to the right subtree
+        root = root->right;
+   }
+   return -1;
+}
