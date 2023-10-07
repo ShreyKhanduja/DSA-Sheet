8b044166c86f50ae50d574a9200b1b9e4a089ea9
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-10-08 04:19:14 +0530

    mixed

--- kth_largest.cpp
+++ kth_largest.cpp
@@ -1,20 +1,12 @@
-int kthLargest(Node *root, int k) {
-    stack<Node*> s;
-    while(!s.empty() || root) {
-        // reach the rightmost node
-        while(root) {
-            s.push(root);
-            root = root->right;
+int findKthLargest(vector<int> &nums, int k) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i = 0; i < nums.size(); i++) {
+        if(pq.size() < k) 
+            pq.push(nums[i]);
+        else if(nums[i] > pq.top()) {
+            pq.pop();
+            pq.push(nums[i]);
         }
-        // pop the rightmost node
-        root = s.top();
-        s.pop();
-        // condition check
-        if(--k == 0)
-            return root->data;
-        // move to the left subtree
-        else    
-            root = root->left;
     }
-    return -1;
+    return pq.top();
 }
