32d7452cb628da0ca9f9dba25964455678a15add
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-10-11 22:42:50 +0530

    mixed

---
+++ kth_largest_stream.cpp
@@ -0,0 +1,18 @@
+
+vector<int> kthLargest(int k, int arr[], int n) {
+    vector<int> results(k - 1, -1); // no window of size k found
+    priority_queue<int, vector<int>, greater<int>> pq; // min heap
+    // iterate the stream of numbers 
+    for(int i = 0; i < k; i++)
+        pq.push(arr[i]);
+
+    results.push_back(pq.top());
+    for(int i = k; i < n; i++) {
+        if(arr[i] > pq.top()) {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+        results.push_back(pq.top());
+    }    
+    return results;
+}
