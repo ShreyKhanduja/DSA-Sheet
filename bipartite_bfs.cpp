0d9e79616d645b2435b9c2865bcf2639e7c2e3a4
Shrey Khanduja <shreykhanduja2001@gmail.com>
2023-10-17 22:39:07 +0530

    Graph bipartite

---
+++ bipartite_bfs.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+
+    bool check_bipartite(int source, vector<int> adj[], vector<int> &color) {
+        queue<int> q;
+        q.push(source);
+        color[source] = 1;
+        // visit the adjacent nodes and mark them with opposite color
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+            
+            for(int &nbr : adj[front]) {
+                // check if not colored/ visited previously
+                if(color[nbr] == -1) {
+                    color[nbr] = 1 - color[front];
+                    // push the neighbor in the queue
+                    q.push(nbr);
+                }
+                // 2 adjacent nodes having the same color: not a bipartite
+                else if(color[nbr] == color[front])
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+	bool isBipartite(int V, vector<int>adj[]){
+	    // Code here
+	    vector<int> color(V, -1);
+	    // init bfs call for each disconnected component
+	    for(int i = 0; i < V; i++) {
+	        if(color[i] == -1) {
+	            if(!check_bipartite(i, adj, color))
+	                return false;
+	        }
+	    }
+	    return true;
+	}
+
+};
